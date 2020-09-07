2a8d0cc5d51d9c5975fe6ecc70ee0219c0f615ba
Salavat Magazov <s.magazov@gmail.com>
2020-09-07 11:47:02 +0100

    code-style: increase tab size to 4.

--- Core/Src/MESChw_setup.c
+++ Core/Src/MESChw_setup.c
@@ -25,23 +25,23 @@
 #include "MESChw_setup.h"
 
 void motor_init() {
-  motor.Rphase = 0;  // We init at 0 to trigger the measurer to get the vals
-  motor.Lphase = 0;  // We init at 0 to trigger the measurer to get the vals
-  motor.uncertainty = 1;
-  motor.RawCurrLim = 3000;
-  motor.RawVoltLim = 2303;
+    motor.Rphase = 0;  // We init at 0 to trigger the measurer to get the vals
+    motor.Lphase = 0;  // We init at 0 to trigger the measurer to get the vals
+    motor.uncertainty = 1;
+    motor.RawCurrLim = 3000;
+    motor.RawVoltLim = 2303;
 }
 
 void hw_init() {
-  g_hw_setup.Rshunt = 0.001;
-  g_hw_setup.RIphPU = 4700;
-  g_hw_setup.RIphSR = 150;
-  g_hw_setup.RVBB = 1500;
-  g_hw_setup.RVBT = 47000;
-  g_hw_setup.OpGain = 16;  // Can this be inferred from the HAL declaration?
-  g_hw_setup.VBGain = g_hw_setup.RVBB / (g_hw_setup.RVBB + g_hw_setup.RVBT);
-  g_hw_setup.Igain =
-      3.3 / (g_hw_setup.Rshunt * 4096 * g_hw_setup.OpGain * g_hw_setup.RIphPU /
-             (g_hw_setup.RIphPU + g_hw_setup.RIphSR));
-  // g_hw_setup.Rshunt*g_hw_setup.OpGain*g_hw_setup.RIphPU/(g_hw_setup.RIphPU+g_hw_setup.RIphSR);
+    g_hw_setup.Rshunt = 0.001;
+    g_hw_setup.RIphPU = 4700;
+    g_hw_setup.RIphSR = 150;
+    g_hw_setup.RVBB = 1500;
+    g_hw_setup.RVBT = 47000;
+    g_hw_setup.OpGain = 16;  // Can this be inferred from the HAL declaration?
+    g_hw_setup.VBGain = g_hw_setup.RVBB / (g_hw_setup.RVBB + g_hw_setup.RVBT);
+    g_hw_setup.Igain =
+        3.3 / (g_hw_setup.Rshunt * 4096 * g_hw_setup.OpGain *
+               g_hw_setup.RIphPU / (g_hw_setup.RIphPU + g_hw_setup.RIphSR));
+    // g_hw_setup.Rshunt*g_hw_setup.OpGain*g_hw_setup.RIphPU/(g_hw_setup.RIphPU+g_hw_setup.RIphSR);
 }
