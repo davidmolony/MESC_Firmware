dd47de9a28d6cc500a880a90ea5c3b0130e97830
David Molony <david.molony@gmail.com>
2020-09-28 10:44:36 +0100

    Working, motor spinning start point

--- Core/Inc/MESCfoc.h
+++ Core/Inc/MESCfoc.h
@@ -25,9 +25,9 @@
 #include "stm32f3xx_hal.h"
 
 #define FOC_SECTORS_PER_REVOLUTION (6)
-#define FOC_CONV_CHANNELS (3)
-#define FOC_TRANSFORMED_CHANNELS (2)
-#define FOC_NUM_ADC (3)
+#define FOC_CONV_CHANNELS          (3)
+#define FOC_TRANSFORMED_CHANNELS   (2)
+#define FOC_NUM_ADC                (3)
 
 // fixme: I think this type of stuff is causing confusion later on especially in
 // the code that strives for maintainability. What does an alias give you?
@@ -37,48 +37,46 @@ typedef float current_amps_t;
 typedef float voltage_t;
 
 // fixme: why is this in a struct?
-typedef struct {
-    foc_angle_t ElecAngle;  // Current electrical angle
-    uint16_t Sector;  // Current electrical sector - 6 sectors, as a consequence
-                      // of Hall and 3 phase sinwave numbered 0-5
-                      // [0,FOC_SECTORS_PER_REVOLUTION)
-    foc_angle_t AnglePerSector;  // 6 sectors per eRevolution ((USHRT_MAX + 1) /
-                                 // FOC_SECTORS_PER_REVOLUTION)
+typedef struct
+{
+    foc_angle_t ElecAngle;  		// Current electrical angle
+    uint16_t Sector;  				// Current electrical sector - 6 sectors, as a consequence of Hall and 3 phase sinwave numbered 0-5
+
+    foc_angle_t AnglePerSector;  	// 6 sectors per eRevolution ((USHRT_MAX + 1) /FOC_SECTORS_PER_REVOLUTION)
+
     foc_angle_t
-        RotorAngle;  // Rotor angle, either fetched from hall sensors as
-                     // (sector*anglePerSector+tim3Count)/6 or from observer
-    foc_angle_t AngleStep;  // At startup, step angle is zero, zero speed. This
-                            // is the angle by which the inverter increments
-                            // each PWM cycle under open loop
+        RotorAngle;  				// Rotor angle, either fetched from hall sensors as (sector*anglePerSector+tim3Count)/6 or from observer
 
-    uint16_t PWM[FOC_CONV_CHANNELS];  // 3 phase vector for the PWM generation,
-                                      // do math on these before writing them to
+    foc_angle_t AngleStep;  		// At startup, step angle is zero, zero speed. This is the angle by which the inverter increments
+                            		// each PWM cycle under open loop
+
+    uint16_t PWM[FOC_CONV_CHANNELS];  // 3 phase vector for the PWM generation, do math on these before writing them to
                                       // the timer registers
+    current_amps_t
+        Iab[FOC_TRANSFORMED_CHANNELS];  // Float vector containing the Clark transformed current in amps
 
     current_amps_t
-        Iab[FOC_TRANSFORMED_CHANNELS];  // Float vector containing the Clark
-                                        // transformed current in amps
-    current_amps_t
-        Idq[FOC_TRANSFORMED_CHANNELS];  // Float vector containing the Park
-                                        // transformed current in amps
-    voltage_t Vbus;  // Float vector containing the bus voltage in Volts
-    voltage_t Vswitch[FOC_CONV_CHANNELS];  // Float vector containing the switch
-                                           // node voltages (phase voltages)
-    foc_field_angle_t FieldAngle;  // Signed number containing the angle between
-                                   // the electrical field and rotor,
-                                   // implemented as rotorAngle-elecAngle
+        Idq[FOC_TRANSFORMED_CHANNELS];  // Float vector containing the Park transformed current in amps
+
+    voltage_t Vbus;  				// Float vector containing the bus voltage in Volts
+    voltage_t Vswitch[FOC_CONV_CHANNELS];  // Float vector containing the switch node voltages (phase voltages)
+
+    foc_field_angle_t FieldAngle;  	// Signed number containing the angle between the electrical field and rotor, implemented
+                                   	// as rotorAngle-elecAngle
 
 } MESCfoc_s;
 
 void foc_init(MESCfoc_s *foc);  // fixme: floating function prototype
 
 // fixme: why is this in a struct? what advantages does this give?
-typedef struct {
+typedef struct
+{
     uint32_t RawADC[FOC_NUM_ADC]
                    [FOC_CONV_CHANNELS];  // ADC1 returns Ucurrent, DClink
-                                         // voltage and U phase voltage ADC2
-                                         // returns Vcurrent, V and W phase
-                                         // voltages ADC3 returns Wcurrent,
+                                         // voltage and U phase voltage
+                                         //  ADC2 returns Vcurrent, V and Wphase
+                                         //  voltages
+                                         // ADC3 returns Wcurrent
     uint16_t ADCOffset[FOC_NUM_ADC];  // During detect phase, need to sense the
                                       // zero current offset
     float ConvertedADC[FOC_NUM_ADC]
@@ -89,7 +87,8 @@ typedef struct {
 
 foc_measurement_t measurement_buffers;  // fixme: floating function prototype
 
-typedef struct {
+typedef struct
+{
     uint16_t Delta;
     uint16_t Length;
 } MESC_RCPWMin_t;
