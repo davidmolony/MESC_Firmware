95e62fba63cfe78573e03a70a668852447715126
Salavat Magazov <s.magazov@gmail.com>
2020-12-28 17:24:27 +0000

    feature: add low level flash handler prototypes.

---
+++ Core/Src/flash_driver.c
@@ -0,0 +1,20 @@
+/*
+ * flash_driver.c
+ *
+ *  Created on: Dec 28, 2020
+ *      Author: salavat.magazov
+ */
+
+uint32_t writeFlash(uint32_t const *const data, uint32_t const count)
+{
+    uint32_t number_written = 0;
+
+    return (count);
+}
+
+uint32_t readFlash(uint32_t *const data, uint32_t const count)
+{
+    uint32_t number_read = 0;
+
+    return (number_read);
+}
