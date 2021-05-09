434db2df342248d3785fa9d72e192349bf70ea7b
cod3b453 <67663711+cod3b453@users.noreply.github.com>
2021-05-09 02:08:00 +0100

    Modified Temperature BIST
    Modified ADC sweep to clamp based on temperature range
    Increased ADC granularity

--- Core/BIST/bist_temp.c
+++ Core/BIST/bist_temp.c
@@ -8,7 +8,38 @@ void bist_temp( void )
 {
     fprintf( stdout, "Starting Temperature BIST\n" );
 
-    for ( uint16_t adc = 0; adc < 4096; adc = adc + 16 )
+    float const Tmin = -20.0f;
+    float const Tmax = 120.0f;
+
+    uint16_t const adc_granularity = UINT16_C(128);
+    uint16_t const adc_mask        = (adc_granularity - 1);
+
+    uint16_t adc_min = temp_get_adc( Tmin );
+    uint16_t adc_max = temp_get_adc( Tmax );
+
+    uint16_t adc_start;
+    uint16_t adc_end;
+
+    if (adc_min < adc_max)
+    {
+        adc_min &= ~adc_mask;
+        adc_max +=  adc_mask;
+        adc_min &= ~adc_mask;
+
+        adc_start = adc_min;
+        adc_end   = adc_max;
+    }
+    else
+    {
+        adc_min +=  adc_mask;
+        adc_min &= ~adc_mask;
+        adc_max &= ~adc_mask;
+
+        adc_start = adc_max;
+        adc_end   = adc_min;
+    }
+
+    for ( uint16_t adc = adc_start; adc < adc_end; adc = adc + adc_granularity )
     {
         float const T = temp_read( adc );
 
