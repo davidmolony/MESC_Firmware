1ff06323d852a9d35d4e09102909e32206488324
DESKTOP-BJ1640A\HPEnvy <david.molony@gmail.com>
2022-07-19 22:15:13 +0100

    Add stacked I2C on the F405 to enable use of screen and stop compilation errors

--- MESC_F405RG/Core/Src/main.c
+++ MESC_F405RG/Core/Src/main.c
@@ -64,6 +64,8 @@ UART_HandleTypeDef huart3;
 DMA_HandleTypeDef hdma_usart3_tx;
 
 /* USER CODE BEGIN PV */
+I2C_HandleTypeDef hi2c2;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -79,6 +81,7 @@ static void MX_USART3_UART_Init(void);
 static void MX_TIM7_Init(void);
 static void MX_SPI3_Init(void);
 /* USER CODE BEGIN PFP */
+static void MX_I2C2_Init(void);
 
 
 /* USER CODE END PFP */
@@ -127,6 +130,8 @@ int main(void)
   MX_TIM7_Init();
   MX_SPI3_Init();
   /* USER CODE BEGIN 2 */
+  //MX_I2C2_Init();
+
   HAL_SPI_Init(&hspi3);
 
   /*
@@ -871,6 +876,39 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
 /* USER CODE END 4 */
 
 /**
