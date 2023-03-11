3feb8224e7927c984e2fe24d4ef77571a9f94738
cod3b453 <67663711+cod3b453@users.noreply.github.com>
2023-03-11 11:08:12 +0000

    Added Arduino ttyUSB workaround
    Forces user-accessible ttyUSB device

---
+++ MESC_Common/UI/Arduino/workaround_ttyusb.bash
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Copyright 2023 cod3b453
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+TTYUSB=${1:-"/dev/ttyUSB0"};
+
+if [ ! -e "${TTYUSB}" ]
+then
+    TTYUSB=''
+fi
+
+if [ $# -eq 0 ] || [ "${TTYUSB}" = "" ]
+then
+    read -p "Enter serial port ('${TTYUSB}'): " TTYUSB2
+
+    if [ "${TTYUSB2}" != "" ]
+    then
+        TTYUSB=${TTYUSB2}
+    fi
+fi
+
+if [ "${TTYUSB}" = "" ] || [ ! -e "${TTYUSB}" ]
+then
+    echo "ERROR: Port '${TTYUSB}' does not exist; aborting"
+    exit 1
+fi
+
+sudo chmod 0777 "${TTYUSB}"
+
