f73489e5411dc93f1e0f8c28035648f546552212
owhite <smouldering.dog@gmail.com>
2023-04-20 20:32:31 -0400

    running on bike

--- MESC_F405RG/Core/Inc/MESC_F405.h
+++ MESC_F405RG/Core/Inc/MESC_F405.h
@@ -1,10 +1,10 @@
 //First, include the header specific to your board, which includes hardware parameters like ABS MAX, shunts, potential divdiders
 //Ensure only one board's header file is uncommented!
-//#include "MP2_V0_1.h"
+#include "MP2_V0_1.h"
 //#include "CL700_V0_3.h"
 //#include "MX_FOC_IMS.h"
 //#include "MX_FOC_GaN.h"
-#include "GIGAVESC.h"
+//#include "GIGAVESC.h"
 
 #define HAS_PHASE_SENSORS //This refers to VOLTAGE sensing on phase, not current!
 
@@ -82,7 +82,7 @@
 
 /////////////////////Related to COMMs INTERFACE//////////////////////////////
 #define MESC_UART_USB 		MESC_USB
-#define HW_UART huart3
+#define HW_UART huart1
 
 /////////////////////Prototype stuff that does not really work nicely//////////////////////////////
 
