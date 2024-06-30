169c6f64b2d2791ce6a9098ead0cad72a855bb1f
Owen White <smouldering.dog@gmail.com>
2024-06-30 17:16:19 -0400

    fix

--- MESC_Common/Inc/MESCfoc.h
+++ MESC_Common/Inc/MESCfoc.h
@@ -216,9 +216,9 @@ typedef struct {
 //extern MESC_raw_typedef motor1;
 
 typedef struct {
-	int Iu;
-	int Iv;
-	int Iw;
+	float Iu;
+	float Iv;
+	float Iw;
 }MESC_offset_typedef;
 
 typedef struct {
