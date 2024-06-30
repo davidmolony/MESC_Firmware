1f7553a19bf2d06340cb276ddd6cdaef1e3ab59f
Owen White <smouldering.dog@gmail.com>
2024-06-30 17:06:14 -0400

    changed how offset.I* are initialized and tracked

--- MESC_Common/Inc/MESC_MOTOR_DEFAULTS.h
+++ MESC_Common/Inc/MESC_MOTOR_DEFAULTS.h
@@ -64,6 +64,14 @@
 #define DEFAULT_MOTOR_R 0.070f //Ohms
 #define DEFAULT_MOTOR_PP 7 //Pole Pairs
 
+#elif defined(FLIPSKY_6382_190KV) //Manually defined on bench, need checking
+#define MAX_MOTOR_PHASE_CURRENT 100.0f
+#define DEFAULT_MOTOR_POWER 50.0f
+#define DEFAULT_FLUX_LINKAGE 0.0045f//Set this to the motor linkage in wB
+#define DEFAULT_MOTOR_Ld 0.000015f //Henries
+#define DEFAULT_MOTOR_Lq 0.000022f//Henries
+#define DEFAULT_MOTOR_R 0.0160f //Ohms -- doesnt account for MOSFETs -- manufacturer said 0.050, wasnt close.
+#define DEFAULT_MOTOR_PP 14 //Pole Pairs
 
 #elif defined(JENS_SERVO)
 
